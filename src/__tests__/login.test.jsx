77d08568df1f40c61f7c5beab90b1209a9698aa1
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-15 12:11:48 +0100

    Add colouring to login component

--- src/__tests__/login.test.jsx
+++ src/__tests__/login.test.jsx
@@ -4,5 +4,5 @@ import Login from "../components/login";
 
 test("renders learn react link", () => {
   render(<Login />);
-  expect(screen.getByText("This is the login screen")).toBeInTheDocument();
+  expect(screen.getByText("phil@awesome-dev")).toBeInTheDocument();
 });
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./login.css";
 
 function Login() {
-  return <div className="login-container">This is the login screen</div>;
+  return (
+    <div className="login-container">
+      <span style={{ color: "lawngreen" }}>phil@awesome-dev</span>:
+      <span style={{ color: "mediumturquoise" }}>~</span>$<span>&#9608;</span>
+    </div>
+  );
 }
 
 export default Login;
