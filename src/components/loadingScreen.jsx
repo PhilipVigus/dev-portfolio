e40c034cf0f06ca413b897e84194280da20b00cc
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-17 09:16:01 +0100

    Fix cursor so it disappears for system messages

--- src/components/loadingScreen.jsx
+++ src/components/loadingScreen.jsx
@@ -3,21 +3,12 @@ import CommandPrompt from "./commandPrompt";
 import Cursor from "./cursor";
 import TypedCommand from "./typedCommand";
 import SystemMessage from "./systemMessage";
+import messageData from "./messageData";
 import "./loadingScreen.css";
 
 function LoadingScreen() {
-  const systemMessages = [
-    { content: "\nDemystifying corporate tax affairs", speed: 300 },
-    { content: "\nWrangling squirrel code", speed: 250 },
-    { content: "\nDiscombobulating ineffective colour choices", speed: 200 },
-    { content: "\nNormalising application uncertainties", speed: 150 },
-    { content: "\nCodifying wibbly line lengths", speed: 120 },
-    { content: "\nFinagling metaphoric page auras", speed: 110 },
-    { content: "\nMulching page debris", speed: 100 },
-    { content: "\nEmbiggening attribute importance", speed: 90 },
-    { content: "\nManipulating skill matrix cromulance", speed: 80 },
-    { content: "\nReady for awesomeness overload", speed: 80 },
-  ];
+  const systemMessages = messageData;
+  const [displayCursor, setDisplayCursor] = useState(true);
 
   const [displayedMessages, setDisplayedMessages] = useState([
     <TypedCommand
@@ -34,6 +25,7 @@ function LoadingScreen() {
       return;
     }
 
+    setDisplayCursor(false);
     const nextMessage = systemMessages.shift();
 
     const messageComponent = (
@@ -57,7 +49,7 @@ function LoadingScreen() {
       <span style={{ padding: "6vw" }}>
         <CommandPrompt />
         {displayedMessages}
-        <Cursor />
+        {displayCursor && <Cursor />}
       </span>
     </div>
   );
