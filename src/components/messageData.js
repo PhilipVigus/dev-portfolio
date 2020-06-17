64b4602a4a0188708ee97cba806f6c2c19dd3683
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-17 09:15:06 +0100

    Extract system message data to its own file

---
+++ src/components/messageData.js
@@ -0,0 +1,14 @@
+const messageData = [
+  { content: "\nDemystifying corporate tax affairs", speed: 70 },
+  { content: "\nWrangling squirrel code", speed: 60 },
+  { content: "\nDiscombobulating ineffective colour choices", speed: 50 },
+  { content: "\nNormalising application uncertainties", speed: 40 },
+  { content: "\nCodifying wibbly line lengths", speed: 30 },
+  { content: "\nFinagling metaphoric page auras", speed: 20 },
+  { content: "\nMulching page debris", speed: 10 },
+  { content: "\nEmbiggening attribute importance", speed: 10 },
+  { content: "\nManipulating skill matrix cromulance", speed: 10 },
+  { content: "\nReady for awesomeness overload", speed: 10 },
+];
+
+export default messageData;
