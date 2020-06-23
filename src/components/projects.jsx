0f219eabb6078d650a0d2a0c95828959ae07e800
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-23 12:08:44 +0100

    Add very basic projects content

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 function Projects() {
-  return <div>Projects page</div>;
+  return (
+    <div>
+      Projects page
+      <div>
+        WhatStack
+        <div>
+          <img width="400px" src="./whatstack-screenshot.png" />
+        </div>
+        <div>
+          A cross between WhatsApp and Stack Overflow. Our final group project
+          at Makers Academy.
+        </div>
+        <div>
+          Using - Javascript, MongoDB, Express, React, Node, WebSockets, Jest,
+          Auth0, React testing library, Heroku, CSS, HTML
+        </div>
+      </div>
+      <div>
+        Trailers
+        <div>
+          <img width="400px" src="./trailers-screenshot.png" />
+        </div>
+        <div>
+          A website I implement to help my wife and I track the films we want to
+          watch based off of trailers we see.
+        </div>
+        <div>
+          Using - Javascript, MongoDB, Express, Node, PUG, Heroku, CSS, HTML
+        </div>
+      </div>
+      <div>
+        Bowling
+        <div>
+          <img width="400px" src="./bowling-screenshot.png" />
+        </div>
+        <div>A client-side bowling card with automated scoring.</div>
+        <div>Using - Javascript, Jasmine, CSS, HTML</div>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
