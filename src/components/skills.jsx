4e2293020a6497496b003691af2fe0d32312395f
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-26 11:48:31 +0100

    Emphasise specific skill styling

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -4,59 +4,69 @@ import "./skills.css";
 function Skills() {
   return (
     <div>
+      <h3>Technical</h3>
       <table className="tech-skills-table">
         <tbody>
           <tr className="tech-skills-row">
             <td className="tech-skills-category">Languages</td>
             <td className="tech-skills-description">
-              My strongest language is JavaScript, but I also have experience
-              with Ruby. One of the main focuses of the course at Makers Academy
-              was teaching us how to learn new languages, and I am confident
-              that I can pick up anything needed quickly and efficiently. In
-              addition, I am also comfortable with HTML and CSS.
+              My strongest language is <strong>JavaScript</strong>, but I also
+              have experience with <strong>Ruby</strong>. One of the main
+              focuses of the course at Makers Academy was teaching us how to
+              learn new languages, and I am confident that I can pick up
+              anything needed quickly and efficiently. In addition, I am also
+              comfortable with <strong>HTML</strong> and <strong>CSS</strong>.
             </td>
           </tr>
           <tr className="tech-skills-row">
             <td className="tech-skills-category">Frameworks</td>
             <td className="tech-skills-description">
-              I am familiar with React and Express, having built a website using
-              the MERN. I have also used Rails and Sinatra.
+              I am familiar with <strong>React</strong> and{" "}
+              <strong>Express</strong>, having built a website using the{" "}
+              <strong>MERN</strong> stack. I have also used{" "}
+              <strong>Rails</strong> and <strong>Sinatra</strong>.
             </td>
           </tr>
           <tr className="tech-skills-row">
             <td className="tech-skills-category">Databases</td>
             <td className="tech-skills-description">
-              I have used basic SQL in conjunction with the PostgreSQL database,
-              and have also worked with MongoDB.
+              I have used basic <strong>SQL</strong> in conjunction with the{" "}
+              <strong>PostgreSQL</strong> database, and have also worked with{" "}
+              <strong>MongoDB</strong>.
             </td>
           </tr>
           <tr className="tech-skills-row">
             <td className="tech-skills-category">Workflow</td>
             <td className="tech-skills-description">
-              I am very familiar with Git, using master, dev and feature
-              branches as part of my day-to-day workflow. All of my work is on
-              Github, which I am comfortable using collaboratively as part of a
-              team with pull requests and code reviews. I have set up CI/CD for
-              several projects using Travis CI and Heroku, and my IDE of choice
-              is VS Code.
+              I am very familiar with <strong>Git</strong>, using master, dev
+              and feature branches as part of my day-to-day workflow. All of my
+              work is on
+              <strong>Github</strong>, which I am comfortable using
+              collaboratively as part of a team with pull requests and code
+              reviews. I have set up <strong>CI/CD</strong> for several projects
+              using <strong>Travis CI</strong> and <strong>Heroku</strong>, and
+              my IDE of choice is <strong>VS Code</strong>.
             </td>
           </tr>
           <tr className="tech-skills-row">
             <td className="tech-skills-category">Testing</td>
             <td className="tech-skills-description">
-              When working with JavaScript, I mainly use Jest, along with the
-              React testing library if I'm developing with the React framework.
-              With Ruby, I use RSpec and Capybara.
+              When working with JavaScript, I mainly use <strong>Jest</strong>,
+              along with the <strong>React testing library</strong> if I'm
+              developing with the React framework, but have also used{" "}
+              <strong>Jasmine</strong>. With Ruby, I use <strong>RSpec</strong>{" "}
+              and <strong>Capybara</strong>.
             </td>
           </tr>
           <tr className="tech-skills-row">
             <td className="tech-skills-category">Methodologies</td>
             <td className="tech-skills-description">
-              I use TDD/BDD on all of my projects as appropriate. All Makers
-              Academy group projects were structured to use Agile/Scrum/XP
-              rituals, so I am familiar working with things like standups,
-              kanban boards and retrospectives. I also understand basic OOP and
-              MVC design principles and practices.
+              I use <strong>TDD/BDD</strong> on all of my projects as
+              appropriate. All Makers Academy group projects were structured to
+              use <strong>Agile/Scrum/XP</strong> rituals, so I am familiar
+              working with things like standups, kanban boards and
+              retrospectives. I also understand basic <strong>OOP</strong> and{" "}
+              <strong>MVC</strong> design principles and practices.
             </td>
           </tr>
         </tbody>
