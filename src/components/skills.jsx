4a1ca786599311c5beee3d25271788de533240dc
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-26 14:27:18 +0100

    Add non-technical skills content

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -4,7 +4,7 @@ import "./skills.css";
 function Skills() {
   return (
     <div>
-      <h3>Technical</h3>
+      <h2 className="skills-heading">Technical</h2>
       <table className="tech-skills-table">
         <tbody>
           <tr className="tech-skills-row">
@@ -51,7 +51,7 @@ function Skills() {
             <td className="tech-skills-category">Testing</td>
             <td className="tech-skills-description">
               When working with JavaScript, I mainly use <strong>Jest</strong>,
-              along with the <strong>React testing library</strong> if I'm
+              along with the <strong>React testing library</strong> if I&apos;m
               developing with the React framework, but have also used{" "}
               <strong>Jasmine</strong>. With Ruby, I use <strong>RSpec</strong>{" "}
               and <strong>Capybara</strong>.
@@ -70,21 +70,43 @@ function Skills() {
           </tr>
         </tbody>
       </table>
-      <h3>Non-technical</h3>
+      <h2 className="skills-heading">Non-technical</h2>
       <table className="tech-skills-table">
         <tbody>
           <tr className="tech-skills-row">
             <td className="tech-skills-category">Communication</td>
             <td className="tech-skills-description">
-              I excel with verbal and written communication, and am able to
-              break complex concepts down into small chunks so they can be
-              easily discussed and understood.
+              <p>
+                Coming from my background as a teacher and then in educational
+                quality assurance, I excel with verbal and written
+                communication, and am able to break complex concepts down into
+                small chunks so they can be easily discussed and understood.
+              </p>
+              <p>
+                I receive regular positive feedback about the way I communicate
+                during group projects and in pair programming. I am also
+                passionate about ensuring everyone gets a say in the direction a
+                project takes, and take pride in my ability to facilitate this.
+              </p>
             </td>
           </tr>
           <tr className="tech-skills-row">
             <td className="tech-skills-category">Problem solving</td>
             <td className="tech-skills-description">
-              I have a thorough, methodical approach to solving problems.
+              <p>
+                I have a thorough, methodical approach to solving problems. With
+                bugs I make minimal and incremental changes that I understand
+                the impact of, which makes it easier to isolate the section of
+                code to focus on.
+              </p>
+              <p>
+                I also have excellent research skills, establishing whether
+                someone else has already solved the same or a similar problem. I
+                check official documentation and read around the subject, making
+                sure I fully understand the issue and its context. I then break
+                the problem down into the smallest possible pieces and solve
+                these micro-problems one at a time.
+              </p>
             </td>
           </tr>
           <tr className="tech-skills-row">
@@ -92,8 +114,24 @@ function Skills() {
               An excellent understanding of how learning works
             </td>
             <td className="tech-skills-description">
-              This helps me to learn quickly, independently and effectively, and
-              allows me to facilitate the learning of those around me.
+              <p>
+                This helps me to learn quickly, independently and effectively,
+                and allows me to facilitate the learning of those around me.
+              </p>
+              <p>
+                Before starting at Makers Academy I created a Trailers website,
+                teaching myself to build and deploy a full stack project using
+                Express, MongoDB and the PUG templating language.
+              </p>
+              <p>
+                At Makers Academy I taught myself React alongside the standard
+                curriculum. I was given regular, extremely positive feedback by
+                team members and pair partners on how well I facilitated their
+                learning without spoonfeeding them solutions. I also mentored
+                two members of a later cohort, helping them get the most out of
+                the course by offering technical and pastoral advice based on my
+                experience.
+              </p>
             </td>
           </tr>
           <tr className="tech-skills-row">
@@ -101,14 +139,47 @@ function Skills() {
               High emotional intelligence
             </td>
             <td className="tech-skills-description">
-              I am extremely self-aware, and am able to empathize effectively
-              with others.
+              <p>
+                I am extremely self-aware, and am able to empathize effectively
+                with others.
+              </p>
+              <p>
+                At the end of one project at Makers Academy there was a split in
+                the group. One side was focused on getting as much done as
+                possible, with one person adding a lot of code that others did
+                not understand. The other side was more interested in ensuring
+                that the work represented the entire group. I identified this
+                problem quickly, and helped the group reach a compromise that
+                everyone was happy with.
+              </p>
+              <p>
+                I give constructive, critical feedback when pair programming,
+                and quickly identify when I make mistakes and am very receptive
+                to feedback on how to improve.
+              </p>
             </td>
           </tr>
           <tr className="tech-skills-row">
             <td className="tech-skills-category">Leadership</td>
             <td className="tech-skills-description">
-              I have strong leadership skills.
+              <p>I have strong leadership skills.</p>
+              <p>
+                At Makers Academy I often took leadership roles during critical
+                times and received unanimous positive feedback on my ability to
+                keep groups on task, ensure that all team members had a voice,
+                and help the team reach consensus when there were disagreements.
+                I led our final project for the first couple of days,
+                establishing solid foundations such as a team charter, consensus
+                on the details of the project, and broadly what the two weeks of
+                the project would look like.
+              </p>
+              <p>
+                In educational quality assurance, I regularly led teams of
+                moderators when visiting clients. I was responsible for
+                coordinating their efforts and ensuring that work was completed
+                to extremely tight schedules in challenging and sometimes
+                unfriendly environments.
+              </p>
             </td>
           </tr>
           <tr className="tech-skills-row">
@@ -116,7 +187,20 @@ function Skills() {
               Determination and persistence
             </td>
             <td className="tech-skills-description">
-              I don&apos;t know when or how to give up.
+              <p>I don&apos;t know when or how to give up.</p>
+              <p>
+                During the final project at Makers Academy the group had huge
+                difficulty working out how to test the WebSocket-based
+                components of the code. We spent hours on it without success. I
+                refused to give up, and finally managed to write a successful
+                test on the morning of the last day of the project.
+              </p>
+              <p>
+                I spent four years unable to work due to ill-health. Recovery
+                was a long, difficult path with plenty of obstacles, but I never
+                once thought that I would not be able to make it back to
+                full-time employment.
+              </p>
             </td>
           </tr>
         </tbody>
