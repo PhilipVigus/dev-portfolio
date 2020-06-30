9cd00442f69959f43fa6aa3627b0072c2a681063
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-30 13:59:28 +0100

    Updates

---
+++ precache-manifest.1c60d188c01df49c89756bc698fa1719.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccad3ec3b1fe2b4ea793193f16d5f6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aabd4cda4ba90d60b6c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ae58c648f938335c9333",
+    "url": "/static/js/2.25eebc30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.25eebc30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aabd4cda4ba90d60b6c",
+    "url": "/static/js/main.0f8c51de.chunk.js"
+  },
+  {
+    "revision": "ac1a04b4f3ac290a19e4",
+    "url": "/static/js/runtime-main.4f40f515.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
