6a461b15b785083c823de17f2356a1daa37bb247
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-05 11:44:09 +0100

    Add hover effect to project summaries

--- src/components/projectSummary.jsx
+++ src/components/projectSummary.jsx
@@ -1,44 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "./projectSummary.css";
 
 function ProjectSummary({ title, summaryText, skills, github, deployLink }) {
-  const [showingDetails, setShowingDetails] = useState(true);
-
-  if (!showingDetails) {
-    return (
-      <div
-        className="project-summary-container__hidden"
-        onMouseLeave={() => {
-          setShowingDetails(true);
-        }}
-      >
-        <h2 className="project-summary-container__heading">{title}</h2>
-        <div className="project-summary-container__text">{summaryText}</div>
-      </div>
-    );
-  } else {
-    return (
-      <div
-        className="project-summary-container__hidden"
-        onMouseEnter={() => {
-          setShowingDetails(false);
-        }}
-      >
-        <h2 className="project-summary-container__heading-hidden">{title}</h2>
-        <div className="project-summary-container__text">
-          <div>{summaryText}</div>
-          <div className="project-summary__skills-container">
-            {skills.map((skill) => (
-              <div className="project-summary__skill" key={skill}>
-                {skill}
-              </div>
-            ))}
-          </div>
+  return (
+    <div className="project-summary-container">
+      <h2 className="project-summary-container__heading">{title}</h2>
+      <div className="project-summary-container__text">
+        <div>{summaryText}</div>
+        <div className="project-summary__skills-container">
+          {skills.map((skill) => (
+            <div className="project-summary__skill" key={skill}>
+              {skill}
+            </div>
+          ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 ProjectSummary.propTypes = {
