45cae8886121169c6d14f9ab11257830ad310f42
PhilipVigus <philipvigus@yahoo.co.uk>
2020-08-06 09:20:41 +0100

    Fix Main component not actually being used (doh!)

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import React from "react";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import LoadingScreen from "./components/loadingScreen";
-import Projects from "./components/projects";
-import NavBar from "./components/navBar";
-import AboutMe from "./components/aboutMe";
+import Main from "./components/main";
 
 function App() {
   return (
     <Router>
       <div className="app-container">
-        <NavBar />
-        <div className="main-content-container">
-          <Route path="/about" exact component={AboutMe} />
-          <Route path="/projects" exact component={Projects} />
-          <Route path="/" exact component={LoadingScreen} />
-        </div>
+        <Main />
       </div>
     </Router>
   );
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,17 +4,18 @@ import LoadingScreen from "./loadingScreen";
 import Projects from "./projects";
 import NavBar from "./navBar";
 import AboutMe from "./aboutMe";
+import "./main.css";
 
 function Main() {
   return (
-    <div>
+    <>
       <NavBar />
       <div className="main-content-container">
         <Route path="/about" exact component={AboutMe} />
         <Route path="/projects" exact component={Projects} />
         <Route path="/" exact component={LoadingScreen} />
       </div>
-    </div>
+    </>
   );
 }
 
